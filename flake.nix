b6f1af3ba8f0ed4e265e5757876338a7c9f700b7
Andy Baizon <dev@baizon.uk>
2025-01-10 00:49:25 +0000

    feat: darwin support

--- flake.nix
+++ flake.nix
@@ -13,38 +13,50 @@
       ...
     }:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          configuredNvim =
+            (nvf.lib.neovimConfiguration {
+              inherit pkgs;
+              modules = [
+                {
+                  vim = {
+                    theme = {
+                      enable = true;
+                      name = "rose-pine";
+                      style = "main";
+                    };
+                    languages = {
+                      enableLSP = true;
+                      enableTreesitter = true;
+                      go.enable = true;
+                      ts.enable = true;
+                      nix.enable = true;
+                      rust.enable = true;
+                      python.enable = true;
+                      markdown.enable = true;
+                    };
+                    statusline.lualine.enable = true;
+                    autocomplete.nvim-cmp.enable = true;
+                    telescope.enable = true;
+                  };
+                }
+              ];
+            }).neovim;
+          default = self.packages.${system}.configuredNvim;
+        }
+      );
     in
     {
-      packages."x86_64-linux".configuredNvim =
-        (nvf.lib.neovimConfiguration {
-          inherit pkgs;
-          modules = [
-            {
-              vim = {
-                theme = {
-                  enable = true;
-                  name = "rose-pine";
-                  style = "main";
-                };
-                languages = {
-                  enableLSP = true;
-                  enableTreesitter = true;
-                  go.enable = true;
-                  ts.enable = true;
-                  nix.enable = true;
-                  rust.enable = true;
-                  python.enable = true;
-                  markdown.enable = true;
-                };
-                statusline.lualine.enable = true;
-                autocomplete.nvim-cmp.enable = true;
-                telescope.enable = true;
-              };
-            }
-          ];
-        }).neovim;
-
-      packages."x86_64-linux".default = self.outputs.packages."x86_64-linux".configuredNvim;
+      packages = packages;
     };
 }
